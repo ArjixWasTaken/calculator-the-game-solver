a3ff74e9ddb3cd354068976dbfbfe218968681d8
nate-moo <40650681+nate-moo@users.noreply.github.com>
2022-06-02 11:44:29 -0400

    refactor levels and add level 62

--- main.py
+++ main.py
@@ -105,297 +105,305 @@ def solve(goal: int, moves: int, start: int, tokens: List[Token]) -> List[List[T
             winning_patterns.pop(seq)
     return winning_patterns
 
+levels = [
+    "Level 0 doesn't exist",
+    solve(2, 2, 0, [Token(1, Operator.addition)]),
+    solve(8, 3, 0, [
+        Token(2, Operator.addition),
+        Token(3, Operator.addition)]
+    ),
+    solve(12, 3, 0, [
+        Token(2, Operator.addition),
+        Token(1, Operator.addition),
+        Token(4, Operator.multiplication)]
+    ),
+    solve(7, 3, 1, [
+        Token(4, Operator.addition),
+        Token(2, Operator.subtraction)]
+    ),
+    solve(20, 3, 0, [
+        Token(4, Operator.multiplication),
+        Token(4, Operator.addition)
+    ]),
+    solve(40, 4, 0, [
+        Token(2, Operator.addition),
+        Token(4, Operator.multiplication)
+    ]),
+    solve(10, 4, 100, [
+        Token(3, Operator.addition),
+        Token(5, Operator.division)
+    ]),
+    solve(4, 3, 4321, [Token(None, Operator.pop)]),
+    solve(4, 3, 0, [
+        Token(None, Operator.pop),
+        Token(8, Operator.addition),
+        Token(5, Operator.multiplication)
+    ]),
+    solve(9, 4, 50, [
+        Token(None, Operator.pop),
+        Token(5, Operator.division),
+        Token(3, Operator.multiplication)
+    ]),
+    solve(100, 3, 99, [
+        Token(None, Operator.pop),
+        Token(8, Operator.subtraction),
+        Token(11, Operator.multiplication)
+    ]),
+    solve(404, 5, 0, [
+        Token(8, Operator.addition),
+        Token(10, Operator.multiplication),
+        Token(2, Operator.division)
+    ]),
+    solve(23, 4, 171, [
+        Token(None, Operator.pop),
+        Token(2, Operator.multiplication),
+        Token(9, Operator.subtraction)
+    ]),
+    solve(21, 5, 0, [
+        Token(None, Operator.pop),
+        Token(5, Operator.addition),
+        Token(3, Operator.multiplication),
+        Token(5, Operator.multiplication)
+    ]),
+    solve(50, 3, 10, [
+        Token(3, Operator.multiplication),
+        Token(2, Operator.multiplication),
+        Token(5, Operator.subtraction)
+    ]),
+    solve(2, 5, 0, [
+        Token(None, Operator.pop),
+        Token(4, Operator.addition),
+        Token(9, Operator.multiplication)
+    ]),
+    solve(11, 2, 0, [Token(1, Operator.insert)]),
+    solve(101, 3, 0, [
+        Token(1, Operator.insert),
+        Token(0, Operator.insert)
+    ]),
+    solve(44, 3, 0, [
+        Token(2, Operator.insert),
+        Token(2, Operator.multiplication)
+    ]),
+    solve(35, 2, 0, [
+        Token(3, Operator.addition),
+        Token(5, Operator.insert)
+    ]),
+    solve(56, 3, 0, [
+        Token(1, Operator.insert),
+        Token(5, Operator.addition)
+    ]),
+    solve(9, 4, 0, [
+        Token(2, Operator.addition),
+        Token(3, Operator.division),
+        Token(1, Operator.insert)
+    ]),
+    solve(10, 4, 15, [
+        Token(0, Operator.insert),
+        Token(2, Operator.addition),
+        Token(5, Operator.division)
+    ]),
+    solve(210, 5, 0, [
+        Token(5, Operator.addition),
+        Token(5, Operator.subtraction),
+        Token(5, Operator.insert),
+        Token(2, Operator.insert),
+    ]),
+    solve(2020, 4, 40, [
+        Token(0, Operator.insert),
+        Token(4, Operator.addition),
+        Token(2, Operator.division),
+    ]),
+    solve(11, 4, 0, [
+        Token(12, Operator.insert),
+        Token(None, Operator.pop)
+    ]),
+    solve(102, 4, 0, [
+        Token(10, Operator.insert),
+        Token(1, Operator.addition),
+        Token(None, Operator.pop)
+    ]),
+    solve(222, 4, 0, [
+        Token(1, Operator.insert),
+        Token((1, 2), Operator.convert)
+    ]),
+    solve(93, 4, 0, [
+        Token(6, Operator.addition),
+        Token(7, Operator.multiplication),
+        Token((6, 9), Operator.convert)
+    ]),
+    solve(2321, 6, 0, [
+        Token(1, Operator.insert),
+        Token(2, Operator.insert),
+        Token((1, 2), Operator.convert),
+        Token((2, 3), Operator.convert),
+    ]),
+    solve(24, 5, 0, [
+        Token(9, Operator.addition),
+        Token(2, Operator.multiplication),
+        Token((8, 4), Operator.convert)
+    ]),
+    solve(29, 5, 11, [
+        Token(2, Operator.division),
+        Token(3, Operator.addition),
+        Token((1, 2), Operator.convert),
+        Token((2, 9), Operator.convert),
+    ]),
+    solve(20, 5, 36, [
+        Token(3, Operator.addition),
+        Token(3, Operator.division),
+        Token((1, 2), Operator.convert)
+    ]),
+    solve(15, 4, 2, [
+        Token(3, Operator.division),
+        Token(1, Operator.insert),
+        Token(2, Operator.multiplication),
+        Token((4, 5), Operator.convert)
+    ]),
+    solve(414, 4, 1234, [
+        Token((23, 41), Operator.convert),
+        Token((24, 14), Operator.convert),
+        Token((12, 24), Operator.convert),
+        Token((14, 2), Operator.convert)
+    ]),
+    solve(-85, 4, 0, [
+        Token(7, Operator.subtraction),
+        Token(6, Operator.addition),
+        Token(5, Operator.insert)
+    ]),
+    solve(9, 3, 0, [
+        Token(1, Operator.subtraction),
+        Token(2, Operator.subtraction),
+        Token(2, Operator.exponential)
+    ]),
+    solve(-120, 4, 0, [
+        Token(5, Operator.multiplication),
+        Token(6, Operator.subtraction),
+        Token(4, Operator.insert)
+    ]),
+    solve(144, 3, 0, [
+        Token(1, Operator.subtraction),
+        Token(2, Operator.insert),
+        Token(2, Operator.exponential)
+    ]),
+    solve(5, 1, -5, [
+        Token(None, Operator.switch)
+    ]),
+    solve(-6, 3, 0, [
+        Token(4, Operator.addition),
+        Token(2, Operator.addition),
+        Token(None, Operator.switch)
+    ]),
+    solve(-13, 4, 0, [
+        Token(3, Operator.addition),
+        Token(7, Operator.subtraction),
+        Token(None, Operator.switch)
+    ]),
+    solve(60, 4, 0, [
+        Token(5, Operator.addition),
+        Token(10, Operator.subtraction),
+        Token(4, Operator.multiplication),
+        Token(None, Operator.switch)
+    ]),
+    solve(52, 5, 44, [
+        Token(9, Operator.addition),
+        Token(2, Operator.division),
+        Token(4, Operator.multiplication),
+        Token(None, Operator.switch)
+    ]),
+    solve(10, 5, 9, [
+        Token(5, Operator.addition),
+        Token(5, Operator.multiplication),
+        Token(None, Operator.switch)
+    ]),
+    solve(12, 5, 14, [
+        Token(6, Operator.insert),
+        Token(5, Operator.addition),
+        Token(8, Operator.division),
+        Token(None, Operator.switch)
+    ]),
+    solve(13, 4, 55, [
+        Token(9, Operator.addition),
+        Token(None, Operator.switch),
+        Token(None, Operator.pop)
+    ]),
+    solve(245, 5, 0, [
+        Token(3, Operator.subtraction),
+        Token(5, Operator.insert),
+        Token(4, Operator.multiplication),
+        Token(None, Operator.switch)
+    ]),
+    solve(12, 4, 39, [
+        Token(-3, Operator.multiplication),
+        Token(3, Operator.division),
+        Token(9, Operator.addition),
+        Token(None, Operator.switch)
+    ]),
+    solve(126, 6, 111, [
+        Token(3, Operator.multiplication),
+        Token(9, Operator.subtraction),
+        Token(None, Operator.switch),
+        Token(None, Operator.pop)
+    ]),
+    solve(3, 5, 34, [
+        Token(5, Operator.subtraction),
+        Token(8, Operator.addition),
+        Token(7, Operator.division),
+        Token(None, Operator.switch)
+    ]),
+    solve(4, 5, 25, [
+        Token(4, Operator.subtraction),
+        Token(-4, Operator.multiplication),
+        Token(3, Operator.division),
+        Token(8, Operator.division),
+        Token(None, Operator.switch)
+    ]),
+    solve(21, 1, 12, [
+        Token(None, Operator.reverse)
+    ]),
+    solve(51, 3, 0, [
+        Token(6, Operator.addition),
+        Token(9, Operator.addition),
+        Token(None, Operator.reverse)
+    ]),
+    solve(101, 3, 100, [
+        Token(1, Operator.insert),
+        Token(9, Operator.addition),
+        Token(None, Operator.reverse),
+    ]),
+    solve(100, 4, 1101, [
+        Token(None, Operator.reverse),
+        Token(1, Operator.subtraction)
+    ]),
+    solve(58, 4, 0, [
+        Token(4, Operator.addition),
+        Token(4, Operator.multiplication),
+        Token(3, Operator.subtraction),
+        Token(None, Operator.reverse)
+    ]),
+    solve(4, 3, 6, [
+        Token(1, Operator.insert),
+        Token(4, Operator.division),
+        Token(None, Operator.reverse)
+    ]),
+    solve(21, 3, 15, [
+        Token(9, Operator.addition),
+        Token(5, Operator.multiplication),
+        Token(None, Operator.reverse)
+    ]),
+    solve(13, 5, 100, [
+        Token(2, Operator.division),
+        Token(None, Operator.reverse)
+    ]), 
+    solve(11011, 4, 10, [
+        Token(None, Operator.reverse),
+        Token(1, Operator.insert)
+    ]),
+    solve(102, 4, 0, [
+        Token(5, Operator.addition),
+        Token(None, Operator.reverse),
+        Token(10, Operator.insert),
+        Token(4, Operator.multiplication)
+    ])
+]
 
-level_one = solve(2, 2, 0, [Token(1, Operator.addition)])
-level_two = solve(8, 3, 0, [
-    Token(2, Operator.addition),
-    Token(3, Operator.addition)]
-)
-level_three = solve(12, 3, 0, [
-    Token(2, Operator.addition),
-    Token(1, Operator.addition),
-    Token(4, Operator.multiplication)]
-)
-level_four = solve(7, 3, 1, [
-    Token(4, Operator.addition),
-    Token(2, Operator.subtraction)]
-)
-level_five = solve(20, 3, 0, [
-    Token(4, Operator.multiplication),
-    Token(4, Operator.addition)
-])
-level_six = solve(40, 4, 0, [
-    Token(2, Operator.addition),
-    Token(4, Operator.multiplication)
-])
-level_seven = solve(10, 4, 100, [
-    Token(3, Operator.addition),
-    Token(5, Operator.division)
-])
-level_eight = solve(4, 3, 4321, [Token(None, Operator.pop)])
-
-level_nine = solve(4, 3, 0, [
-    Token(None, Operator.pop),
-    Token(8, Operator.addition),
-    Token(5, Operator.multiplication)
-])
-level_ten = solve(9, 4, 50, [
-    Token(None, Operator.pop),
-    Token(5, Operator.division),
-    Token(3, Operator.multiplication)
-])
-level_eleven = solve(100, 3, 99, [
-    Token(None, Operator.pop),
-    Token(8, Operator.subtraction),
-    Token(11, Operator.multiplication)
-])
-level_twelve = solve(404, 5, 0, [
-    Token(8, Operator.addition),
-    Token(10, Operator.multiplication),
-    Token(2, Operator.division)
-])
-level_thirteen = solve(23, 4, 171, [
-    Token(None, Operator.pop),
-    Token(2, Operator.multiplication),
-    Token(9, Operator.subtraction)
-])
-level_fourteen = solve(21, 5, 0, [
-    Token(None, Operator.pop),
-    Token(5, Operator.addition),
-    Token(3, Operator.multiplication),
-    Token(5, Operator.multiplication)
-])
-level_fifteen = solve(50, 3, 10, [
-    Token(3, Operator.multiplication),
-    Token(2, Operator.multiplication),
-    Token(5, Operator.subtraction)
-])
-level_sixteen = solve(2, 5, 0, [
-    Token(None, Operator.pop),
-    Token(4, Operator.addition),
-    Token(9, Operator.multiplication)
-])
-level_seventeen = solve(11, 2, 0, [Token(1, Operator.insert)])
-level_eighteen = solve(101, 3, 0, [
-    Token(1, Operator.insert),
-    Token(0, Operator.insert)
-])
-level_nineteen = solve(44, 3, 0, [
-    Token(2, Operator.insert),
-    Token(2, Operator.multiplication)
-])
-level_twenty = solve(35, 2, 0, [
-    Token(3, Operator.addition),
-    Token(5, Operator.insert)
-])
-level_twenty_one = solve(56, 3, 0, [
-    Token(1, Operator.insert),
-    Token(5, Operator.addition)
-])
-level_twenty_two = solve(9, 4, 0, [
-    Token(2, Operator.addition),
-    Token(3, Operator.division),
-    Token(1, Operator.insert)
-])
-level_twenty_three = solve(10, 4, 15, [
-    Token(0, Operator.insert),
-    Token(2, Operator.addition),
-    Token(5, Operator.division)
-])
-level_twenty_four = solve(210, 5, 0, [
-    Token(5, Operator.addition),
-    Token(5, Operator.subtraction),
-    Token(5, Operator.insert),
-    Token(2, Operator.insert),
-])
-level_twenty_five = solve(2020, 4, 40, [
-    Token(0, Operator.insert),
-    Token(4, Operator.addition),
-    Token(2, Operator.division),
-])
-level_twenty_six = solve(11, 4, 0, [
-    Token(12, Operator.insert),
-    Token(None, Operator.pop)
-])
-level_twenty_seven = solve(102, 4, 0, [
-    Token(10, Operator.insert),
-    Token(1, Operator.addition),
-    Token(None, Operator.pop)
-])
-level_twenty_eight = solve(222, 4, 0, [
-    Token(1, Operator.insert),
-    Token((1, 2), Operator.convert)
-])
-level_twenty_nine = solve(93, 4, 0, [
-    Token(6, Operator.addition),
-    Token(7, Operator.multiplication),
-    Token((6, 9), Operator.convert)
-])
-level_thirty = solve(2321, 6, 0, [
-    Token(1, Operator.insert),
-    Token(2, Operator.insert),
-    Token((1, 2), Operator.convert),
-    Token((2, 3), Operator.convert),
-])
-level_thirty_one = solve(24, 5, 0, [
-    Token(9, Operator.addition),
-    Token(2, Operator.multiplication),
-    Token((8, 4), Operator.convert)
-])
-level_thrity_two = solve(29, 5, 11, [
-    Token(2, Operator.division),
-    Token(3, Operator.addition),
-    Token((1, 2), Operator.convert),
-    Token((2, 9), Operator.convert),
-])
-level_thirty_three = solve(20, 5, 36, [
-    Token(3, Operator.addition),
-    Token(3, Operator.division),
-    Token((1, 2), Operator.convert)
-])
-level_thirty_four = solve(15, 4, 2, [
-    Token(3, Operator.division),
-    Token(1, Operator.insert),
-    Token(2, Operator.multiplication),
-    Token((4, 5), Operator.convert)
-])
-level_thirty_five = solve(414, 4, 1234, [
-    Token((23, 41), Operator.convert),
-    Token((24, 14), Operator.convert),
-    Token((12, 24), Operator.convert),
-    Token((14, 2), Operator.convert)
-])
-level_thirty_six = solve(-85, 4, 0, [
-    Token(7, Operator.subtraction),
-    Token(6, Operator.addition),
-    Token(5, Operator.insert)
-])
-level_thirty_seven = solve(9, 3, 0, [
-    Token(1, Operator.subtraction),
-    Token(2, Operator.subtraction),
-    Token(2, Operator.exponential)
-])
-level_thirty_eight = solve(-120, 4, 0, [
-    Token(5, Operator.multiplication),
-    Token(6, Operator.subtraction),
-    Token(4, Operator.insert)
-])
-level_thirty_nine = solve(144, 3, 0, [
-    Token(1, Operator.subtraction),
-    Token(2, Operator.insert),
-    Token(2, Operator.exponential)
-])
-level_fourty = solve(5, 1, -5, [
-    Token(None, Operator.switch)
-])
-level_fourty_one = solve(-6, 3, 0, [
-    Token(4, Operator.addition),
-    Token(2, Operator.addition),
-    Token(None, Operator.switch)
-])
-level_fourty_two = solve(-13, 4, 0, [
-    Token(3, Operator.addition),
-    Token(7, Operator.subtraction),
-    Token(None, Operator.switch)
-])
-level_fourty_three = solve(60, 4, 0, [
-    Token(5, Operator.addition),
-    Token(10, Operator.subtraction),
-    Token(4, Operator.multiplication),
-    Token(None, Operator.switch)
-])
-level_fourty_four = solve(52, 5, 44, [
-    Token(9, Operator.addition),
-    Token(2, Operator.division),
-    Token(4, Operator.multiplication),
-    Token(None, Operator.switch)
-])
-level_fourty_five = solve(10, 5, 9, [
-    Token(5, Operator.addition),
-    Token(5, Operator.multiplication),
-    Token(None, Operator.switch)
-])
-level_fourty_six = solve(12, 5, 14, [
-    Token(6, Operator.insert),
-    Token(5, Operator.addition),
-    Token(8, Operator.division),
-    Token(None, Operator.switch)
-])
-level_fourty_seven = solve(13, 4, 55, [
-    Token(9, Operator.addition),
-    Token(None, Operator.switch),
-    Token(None, Operator.pop)
-])
-level_fourty_eight = solve(245, 5, 0, [
-    Token(3, Operator.subtraction),
-    Token(5, Operator.insert),
-    Token(4, Operator.multiplication),
-    Token(None, Operator.switch)
-])
-level_fourty_nine = solve(12, 4, 39, [
-    Token(-3, Operator.multiplication),
-    Token(3, Operator.division),
-    Token(9, Operator.addition),
-    Token(None, Operator.switch)
-])
-level_fifty = solve(126, 6, 111, [
-    Token(3, Operator.multiplication),
-    Token(9, Operator.subtraction),
-    Token(None, Operator.switch),
-    Token(None, Operator.pop)
-])
-level_fifty_one = solve(3, 5, 34, [
-    Token(5, Operator.subtraction),
-    Token(8, Operator.addition),
-    Token(7, Operator.division),
-    Token(None, Operator.switch)
-])
-level_fifty_two = solve(4, 5, 25, [
-    Token(4, Operator.subtraction),
-    Token(-4, Operator.multiplication),
-    Token(3, Operator.division),
-    Token(8, Operator.division),
-    Token(None, Operator.switch)
-])
-level_fifty_three = solve(21, 1, 12, [
-    Token(None, Operator.reverse)
-])
-level_fifty_four = solve(51, 3, 0, [
-    Token(6, Operator.addition),
-    Token(9, Operator.addition),
-    Token(None, Operator.reverse)
-])
-level_fifty_five = solve(101, 3, 100, [
-    Token(1, Operator.insert),
-    Token(9, Operator.addition),
-    Token(None, Operator.reverse),
-])
-level_fifty_six = solve(100, 4, 1101, [
-    Token(None, Operator.reverse),
-    Token(1, Operator.subtraction)
-])
-level_fifty_seven = solve(58, 4, 0, [
-    Token(4, Operator.addition),
-    Token(4, Operator.multiplication),
-    Token(3, Operator.subtraction),
-    Token(None, Operator.reverse)
-])
-level_fifty_eight = solve(4, 3, 6, [
-    Token(1, Operator.insert),
-    Token(4, Operator.division),
-    Token(None, Operator.reverse)
-])
-level_fifty_nine = solve(21, 3, 15, [
-    Token(9, Operator.addition),
-    Token(5, Operator.multiplication),
-    Token(None, Operator.reverse)
-])
-level_sixty = solve(13, 5, 100, [
-    Token(2, Operator.division),
-    Token(None, Operator.reverse)
-])
-level_sixty_one = solve(11011, 4, 10, [
-    Token(None, Operator.reverse),
-    Token(1, Operator.insert)
-])
-print(level_sixty_one)
+print(levels[-1])
